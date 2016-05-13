c9035b7023a6c6a98a0cadcbb8e812e37809c48b
Anita Owens <anitaowens@Anita-Owens-MacBook-Air.local>
2016-05-13 23:46:12 +0200

    Continuing exercise 2

--- where.statements.sql
+++ where.statements.sql
@@ -1,3 +1,30 @@
+The first thing you should do every time you start working with a database: load the SQL library
+Since Jupyter is run through Python, the first thing you need to do to start practicing SQL queries is load the SQL library. To do this, type the following line of code into the empty cell below:
+%load_ext sql
+
+
+%sql SHOW tables
+
+The syntax, which sounds very similar to what you would actually say in the spoken English language, looks like this:
+
+SHOW columns FROM (enter table name here)
+
+or if you have multiple databases loaded:
+
+SHOW columns FROM (enter table name here) FROM (enter database name here)
+or
+
+SHOW columns FROM databasename.tablename
+
+%sql SHOW columns FROM dogs
+
+DESCRIBE tablename
+%sql DESCRIBE reviews
+
+EXAMPLE:
+The following SQL statement selects all the columns from the "Customers" table:
+SELECT * FROM Customers;
+
 %%sql
 SELECT user_guid, free_start_user
 FROM users
@@ -68,3 +95,62 @@ SELECT *
 FROM users
 WHERE state IN ("NC", "NY")
 LIMIT 10
+
+/*Question 4: Now that you have seen how datetime data can be used to impose criteria on the data
+you select, how would you select all the Dog IDs and time stamps of Dognition tests completed before 
+October 15, 2015 (your output should have 193,246 rows)?
+*/
+%%sql
+SELECT dog_guid, created_at
+FROM complete_tests
+WHERE created_at < '2015-10-15'
+LIMIT 10
+
+
+%%sql
+SELECT COUNT(*)
+FROM
+(SELECT dog_guid, created_at
+FROM complete_tests
+WHERE created_at < '2015-10-15')
+myNewTable;
+
+
+/***Question 5: How would you select all the User IDs of customers who do not have null 
+values in the State field of their demographic information (if you do not limit the output, 
+you should get 17,985 from this query -- there are a lot of null values in the state field!)?**
+*/
+
+%%sql
+SELECT COUNT(*)
+FROM
+(SELECT user_guid
+FROM users
+WHERE state IS NOT NULL)
+myNewTable;
+
+
+/*Question 6: How would you retrieve the Dog ID, subcategory_name, and test_name fields,
+in that order, of the first 10 reviews entered in the Reviews table to be submitted in 2014?
+*/
+%%sql
+SHOW columns FROM reviews
+
+%%sql
+SELECT dog_guid, subcategory_name, test_name, created_at
+FROM reviews
+WHERE created_at  > '2014-01-01'
+LIMIT 10;
+
+/*
+Question 7: How would you select all of the User IDs of customers who have
+ female dogs whose breed includes the word "terrier" somewhere in its name
+  (if you don't limit your output, you should have 1771 rows in your output)?
+*/
+
+%%sql
+SHOW columns FROM users
+SELECT user_guid
+FROM customers
+WHERE 
+LIMIT 10;
